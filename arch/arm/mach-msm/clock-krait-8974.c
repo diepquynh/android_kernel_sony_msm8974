ea077fd436a272fb122813b82c4f8ca3c75495d9
Remilia Scarlet <remilia.1505@gmail.com>
2018-05-18 19:11:14 +0700

    msm8974: Reduce OC to 2,26GHz
    A high overclock value causes system unstability issues (in my case
    which is randomly shutdown while gaming, even with high voltage set
    on 2,5GHz OC freq)
    
    Also, we need more battery backup than performance for 8974, since
    it's already a high-end CPU (at least before)
    
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- arch/arm/mach-msm/clock-krait-8974.c
+++ arch/arm/mach-msm/clock-krait-8974.c
@@ -474,18 +474,11 @@ static void get_krait_bin_format_b(struct platform_device *pdev,
 
 	dev_info(&pdev->dev, "PVS version: %d\n", *pvs_ver);
 
-#ifdef CONFIG_CPU_OC
-#ifdef CONFIG_OC_ULTIMATE
+	/* From @DooMLoRD: Force speed3 OC 2,26GHz */
 	dev_info(&pdev->dev, "DooMLoRD: Forcing CPU OC Ultimate!\n");
 	*speed = 3;
 #ifdef CONFIG_MACH_SONY_RHINE
 	*pvs = 4;
-#endif
-#else
-	dev_info(&pdev->dev, "DooMLoRD: Forcing CPU OC!\n");
-	*speed = 1;
-#endif
-	dev_info(&pdev->dev, "CPU OC: Speed bin: %d\n", *speed);
 #endif
 
 	devm_iounmap(&pdev->dev, base);
